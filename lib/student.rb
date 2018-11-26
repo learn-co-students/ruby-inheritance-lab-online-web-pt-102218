6f4c60414ee9ef1b45f539638de7fd2efd07b3d9
Matthew McCormack <mmccormack1182@gmail.com>
2018-11-26 10:24:21 -0500

    Complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+
+  attr_writer :knowledge
+
+  def initialize
+    self.knowledge = []
+  end
+
+  def learn(string)
+    @knowledge.push(string)
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
