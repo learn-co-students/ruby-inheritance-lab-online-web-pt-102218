66634c6065298fcc19277bc42d25a34c833dabb8
Paul Ashour <paul.ashour@gmail.com>
2018-11-28 16:18:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(k_str)
+    @knowledge << k_str
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
