529a6a0c5b197a29f6f39c31191adc5bab698919
Anysia V <anysiavalko@gmail.com>
2019-01-16 18:42:13 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,9 @@
-class Teacher
+class Teacher < User
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  KNOWLEDGE = ["Junior Mints are delicious", "Starbursts will break your teeth", "Skittles aren't the rainbow if they don't have blue", "Nerds are just not worth the trouble of spilling them everywhere", "Red Vines are better than Twizzlers", "Hershey Kisses aren't worth the trouble of opening the wrapper", "Cotton Candy is disgusting", "How is Baby Ruth still being made?", "Swedish Fish are addicting", "Kit Kats never get old", "Almond Joy over Mounds every time", "York Peppermint patties are yum" ]
 
+  def teach 
+    KNOWLEDGE.sample
+  end
   
 end
